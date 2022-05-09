4d33da5f0349ad38de47423582f7ced63c5a8bb8
cyrille <aviateur22@hotmail.fr>
2022-05-09 16:39:19 +0200

    starting user_role_privilege upgrade

--- src/components/userInformation/UserRole.vue
+++ src/components/userInformation/UserRole.vue
@@ -3,25 +3,37 @@
     <section class="information__title-container">
         <h2 class="information__title">Votre position </h2>
     </section>
-    <!-- utilisateur -->
+    <!-- role utilisateurutilisateur -->
     <section class="role__container">
         <div class="role__text-container">
             <span class="role__text">vous êtes un <strong class="role__text-strong">{{userRole}}</strong></span>
-        </div>        
+        </div>   
+        <!-- upgrade du status -->
+        <div v-if="this.getUserRole <= 1" class="upgrade__container">
+            <SubmitButton textSubmitButton="devenir éditeur de leçons"/>
+        </div>                   
     </section>
 </template>
 
 <script>
+import SubmitButton from '../button/SubmitButton.vue';
 export default {
     name: 'userRole',
+    components: {
+        SubmitButton
+    },
     computed: {
-        /** role utilisateur */
+        /**role utilisateur */
+        getUserRole(){
+            return this.$store.getters.getUserIdent.roleId;
+        },
+
+        /** role utilisateur en formatet text */
         // eslint-disable-next-line vue/return-in-computed-property
         userRole(){
-            const roleId = this.$store.getters.getUserIdent.roleId;
-            switch (roleId){
+            switch (this.getUserRole){
             case 1: return 'utilisateur';
-            case 2: return 'compositeur';
+            case 2: return 'éditeur';
             case 3: return 'admin';
             case 4: return 'superAdmin';
             default: return 'utilisateur';
@@ -47,6 +59,7 @@ export default {
 
     .role__container{      
         display: flex;
+        flex-direction: column;
         align-items: center;
         justify-content: center; 
         width: 100%; 
@@ -63,6 +76,10 @@ export default {
         border-radius: 10px;
         width: 100%;
         padding: 20px 0px;
+    }
+
+    .upgrade__container{
+        width: 100%
     }
 
     .role__text{
